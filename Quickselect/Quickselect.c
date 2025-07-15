95b613708e94b5652bf29a5b743b52da32f42979
Conner Lavineway <63662176+Conner-Lavineway@users.noreply.github.com>
2025-07-15 18:26:00 -0500

    updates

--- Quickselect/Quickselect.c
+++ Quickselect/Quickselect.c
@@ -6,11 +6,11 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-
-
+#include <time.h>
+#include <ctype.h>
 
 #define MAX_INPUT 10
-#define MAX_LENGTH 10
+#define MAX_LENGTH 500
 #define MAX_NUM 1000
 #define MIN_NUM 1
 
@@ -20,7 +20,7 @@ void swap(int* a, int* b);
 void printArray(int arr[], int start, int end);
 
 //calls everything else
-void main()
+int main()
 {
     srand(time(NULL));
 
@@ -31,7 +31,8 @@ void main()
         numList[i] = rand() % MAX_NUM + MIN_NUM;
     }
 
-    printf("A list of 500 random integers from 1 to 1000 has been made\n");
+    printf("A list of 500 random integers from 1 to 1000 has been made. Here it is:\n");
+    printArray(numList, 0, MAX_LENGTH);
     printf("Now give me a number if it is in the list I will tell you where it is\n");
 
     char input[MAX_INPUT];
@@ -55,9 +56,11 @@ void main()
                 printf("Your number isn't in the list but I'll check anyways\n");
         }
     }
-    int pos = quickSelect(val, numList, 0, MAX_LENGTH-1);
+    printArray(numList, 0, MAX_LENGTH);
+    int pos = quickSelect(val, numList, 0, MAX_LENGTH);
     printf("Your value is located at %d\n", pos);
     printArray(numList, 0, MAX_LENGTH);
+    return pos;
 
 }
 
@@ -65,8 +68,13 @@ void main()
 void printArray(int arr[], int start, int end)
 {
     for(int i = start; i < end; i++)
+    {
         printf("%d, ", arr[i]);
-        printf("\n");
+        if(i % MAX_INPUT == 0 && i != 0)
+            printf("\n");
+    }
+
+    printf("\n\n");
 }
 /**
  * We need to first partition the array around a chosen pivot, this will be 
@@ -80,13 +88,19 @@ void printArray(int arr[], int start, int end)
  //TODO: Figure out how to return -1 if not in list
 int quickSelect(int val, int arr[], int low, int high)
 {
+    //I dont think this is right
+    if(low >= high)
+    {
+        printf("Number not found in list\n");
+        return -1;
+    }
     if(arr[low] == val)
     {
         printArray(arr, low, high);  
         return low;
     }
 
-    int pivot = arr[high];
+    int pivot = arr[high-1];
 
     int p = low;
 
@@ -94,7 +108,7 @@ int quickSelect(int val, int arr[], int low, int high)
     {
         if(arr[i] < pivot)
         {
-            if(i == p)
+            if(p == i)
             {
                 p++;
                 break;
@@ -106,7 +120,7 @@ int quickSelect(int val, int arr[], int low, int high)
     }
     
     printArray(arr, low, high);
-    swap(&arr[p], &arr[high]);
+    swap(&arr[p], &arr[high-1]);
     printArray(arr, low, high);
     
     if(arr[p] == val)
